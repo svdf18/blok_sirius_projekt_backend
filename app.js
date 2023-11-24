6d7b8c14ea17d6c21996329e4e54f1423661d26b
Rolf <rarogbennu@gmail.com>
2023-11-24 09:07:07 +0100

    app

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { userRouter } from "./routes/userRouter.js";
+import { preferenceRouter } from "./routes/preferenceRouter.js";
+import { departmentRouter } from "./routes/departmentRouter.js";
 
 const app = express();
-const port = process.env.PORT || 3306;
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRouter);
+app.use("/preferences", preferenceRouter);
+app.use("/departments", departmentRouter);
+// app.use("/posts", postRouter);
+
 
 app.get("/", (req, res) => {
   res.send("Users");
